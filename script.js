2f4d3f78b0bf250065d1a42fb22337b6cd4a811c
Dylan Drechsel <drechseldylan@gmail.com>
2021-11-15 18:26:37 -0500

    Completed pipe styles

--- script.js
+++ script.js
@@ -33,12 +33,15 @@ function checkLose() {
 function handleStart() {
     title.classList.add("hide")
     setupBird()
+    lastTime = null
     window.requestAnimationFrame(updateLoop)
 }
 
 function handleLose() {
-    title.classList.remove("hide")
-    subtitle.classList.remove("hide")
-    subtitle.textContent = " 0 Pipes"
-    document.addEventListener("keypress", handleStart, { once: true })
+    setTimeout(() => {
+        title.classList.remove("hide")
+        subtitle.classList.remove("hide")
+        subtitle.textContent = " 0 Pipes"
+        document.addEventListener("keypress", handleStart, { once: true })
+    }, 100)
 }

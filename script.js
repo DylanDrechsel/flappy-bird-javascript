eef86418d62d5ddf5b12eb34afb2e4c88a400778
Dylan Drechsel <drechseldylan@gmail.com>
2021-11-15 17:39:09 -0500

    calculated the delta between each rerender to make sure the fall animation works properly

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 document.addEventListener("keypress", handleStart, { once: true })
 const title = document.querySelector("[data-title]")
+let lastTime
+
+/* need to calculate the delta between each rerender to maker sure fall animation works properly regardless if the page takes 10sec or 30sec */
+function updateLoop(time) {
+    /* skips the first render */
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(updateLoop)
+        return
+    }
+
+    const delta = time - lastTime
+    lastTime = time
+    window.requestAnimationFrame(updateLoop)
+}
 
 function handleStart() {
     title.classList.add("hide")
+    window.requestAnimationFrame(updateLoop)
 }
 
 function handleClose() {
-    
+
 }

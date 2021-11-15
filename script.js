1cf57a0a3d538d0a0f25dbb23a39fda754279973
Dylan Drechsel <drechseldylan@gmail.com>
2021-11-15 17:27:10 -0500

    sanity test for handleStart function works

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("keypress", handleStart, { once: true })
+const title = document.querySelector("[data-title]")
+
+function handleStart() {
+    title.classList.add("hide")
+}
+
+function handleClose() {
+    
+}
